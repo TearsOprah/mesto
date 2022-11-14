0f4785d665830c4d0b4ae57f6438f861e5c8a115
TearsOprah <qmeshokp@mail.ru>
2022-11-14 09:17:54 +0200

    fix: исправить ошибки первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -9,43 +9,27 @@ let formElement = document.querySelector('.popup__container');
 let nameInput = formElement.querySelector('#nameInput');
 let jobInput = formElement.querySelector('#jobInput');
 
-// заполняем поля формы значениями со страницы
-nameInput.setAttribute('value', profileTitleElement.textContent);
-jobInput.setAttribute('value', profileSubtitleElement.textContent);
-
 // редактирование имени и информации
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileTitleElement.textContent = nameInput.value;
   profileSubtitleElement.textContent = jobInput.value;
-  togglePopupVisibility();
+  closePopup();
 }
 
-// закрытие и открытие попапа
-const togglePopupVisibility = function() {
-  popupElement.classList.toggle('popup_opened');
+// закрытие попапа
+const closePopup = function() {
+  popupElement.classList.remove('popup_opened');
 }
 
-// закрытие попапа по клику на оверлей
-const closePopupByClickOnOverlay = function(event) {
-  if (event.target !== event.currentTarget) {
-    return;
-  }
-  popupElement.classList.toggle('popup_opened');
+// открытие попапа
+const openPopup = function () {
+  // заполняем поля формы значениями со страницы
+  nameInput.setAttribute('value', profileTitleElement.textContent);
+  jobInput.setAttribute('value', profileSubtitleElement.textContent);
+  popupElement.classList.add('popup_opened');
 }
 
-// сохранение данных по нажанию Enter
-const saveFormByClickEnter = function(event) {
-  if (event.key === 'Enter' && popupElement.classList.contains('popup_opened')) {
-    event.preventDefault();
-    profileTitleElement.textContent = nameInput.value;
-    profileSubtitleElement.textContent = jobInput.value;
-    togglePopupVisibility();
-  }
-}
-
-popupOpenButtonElement.addEventListener('click', togglePopupVisibility);
-popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
-popupElement.addEventListener('click', closePopupByClickOnOverlay);
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
-document.addEventListener('keypress', saveFormByClickEnter);
