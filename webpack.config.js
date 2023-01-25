b56509c6df0952c1dfbe3370b895bcf7f559eb62
TearsOprah <qmeshokp@mail.ru>
2023-01-25 15:11:19 +0200

    fix: установка и настройка babel

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports = {
   module: {
     rules: [
       {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      },
+      {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader, {
